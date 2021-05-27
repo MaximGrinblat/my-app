24f6db97f656be1feedced42e1f338511fe5a4c5
danlasker <dan.lasker12@gmail.com>
2021-05-27 16:20:28 -0700

    Done

--- src/app/components/graph/graph.component.ts
+++ src/app/components/graph/graph.component.ts
@@ -13,8 +13,24 @@ export class GraphComponent implements OnInit {
 
   highcharts: typeof Highcharts = Highcharts;
   data = [];
-  options = {};
+  options: Highcharts.Options = {
+    series: []
+  };
   selectedIdx = -1;
+  updateFlag: true;
+
+  example = [{"start": 11, "end": 13, "description": "A-B"},
+    {"start": 11, "end": 120, "description": "B-F"},
+    {"start": 11, "end": 13, "description": "C-D"},
+    {"start": 11, "end": 13, "description": "D-E"},
+    {"start": 11, "end": 13, "description": "E-F"},
+    {"start": 11, "end": 13, "description": "F-A"}
+  ];
+  parsed: any;
+  matchedChart: any;
+  matchedChartIdx = -1;
+  startIdx: number;
+  endIdx: number;
 
   constructor(private algService: AlgorithmService) {
   }
@@ -24,21 +40,23 @@ export class GraphComponent implements OnInit {
     this.data = [
       {
         name: this.algService.selectedChart,
-        data: this.algService.table[this.selectedIdx]
+        data: this.getCol(this.algService.table, this.selectedIdx)
       },
       {
-        name: 'Nicesnippets.com',
-        data: [677, 455, 677, 877, 455, 778, 888, 567, 785, 488, 567, 654]
-      }];
+        name: this.matchedChart,
+        data: this.getCol(this.algService.table, this.matchedChartIdx)
+      },
+      {
+        name: "Anomaly",
+        data: [[this.startIdx, 50], [this.endIdx, 50]]
+      }
+    ];
     this.options = {
       chart: {
         type: 'spline'
       },
       title: {
         text: 'Monthly Site Visitor'
-      },
-      xAxis: {
-        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
       },
       yAxis: {
         title: {
@@ -49,12 +67,40 @@ export class GraphComponent implements OnInit {
     };
   }
 
+  rsplit(st, sep, maxsplit) {
+    var split = st.split(sep);
+    return maxsplit ? [split.slice(0, -maxsplit).join(sep)].concat(split.slice(-maxsplit)) : split;
+  }
+
   displayChart(): void {
-    for (this.selectedIdx = 0; this.selectedIdx < 100; this.selectedIdx++) {
-      if (this.algService.table[0][this.selectedIdx] == this.algService.selectedChart) {
-        break;
+    let parsed = [];
+    for (let i = 0; i < this.algService.table[0].length; i++) {
+      parsed = this.rsplit(this.example[i]['description'], "-", 1);
+      if (parsed[0] == this.algService.selectedChart) {
+        this.matchedChart = parsed[1];
+        this.startIdx = this.example[i]["start"];
+        this.endIdx = this.example[i]["end"];
       }
     }
+    this.matchedChartIdx = this.getIdxChart(this.algService.table, this.matchedChart);
+    this.selectedIdx = this.getIdxChart(this.algService.table, this.algService.selectedChart);
     this.ngOnInit();
+  }
+
+  getIdxChart(matrix, chart) {
+    let i = 0;
+    for (i = 0; i < matrix.length; i++) {
+      if (matrix[0][i] == chart) {
+        return i;
+      }
+    }
+  }
+
+  getCol(matrix, col) {
+    var column = [];
+    for (var i = 1; i < matrix.length; i++) {
+      column.push(parseInt(matrix[i][col]));
+    }
+    return column;
   }
 }
