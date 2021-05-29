efbcc018f9cb9d09ec8bef40d2dfb5c736d9beab
danlasker <dan.lasker12@gmail.com>
2021-05-29 02:08:46 -0700

    added http request, needs to interspect response

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { AppComponent } from './app.component';
-import { SelectALGComponent } from './components/select-alg/select-alg.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { FileDragNDropDirective } from './directives/file-drag-n-drop.directive';
-import { DropZoneComponent } from './components/drop-zone/drop-zone.component';
-import { DialogConfirmComponent } from './components/dialog-confirm/dialog-confirm.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {AppComponent} from './app.component';
+import {SelectALGComponent} from './components/select-alg/select-alg.component';
+import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {FileDragNDropDirective} from './directives/file-drag-n-drop.directive';
+import {DropZoneComponent} from './components/drop-zone/drop-zone.component';
+import {DialogConfirmComponent} from './components/dialog-confirm/dialog-confirm.component';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatListModule} from '@angular/material/list';
 import {MatButtonModule} from '@angular/material/button';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatDialogModule} from '@angular/material/dialog';
-import { TableComponent } from './components/table/table.component';
-import { FormsModule } from '@angular/forms';
-import { NgxCsvParserModule } from 'ngx-csv-parser';
-import { HighchartsChartModule } from 'highcharts-angular';
-import { GraphComponent } from './components/graph/graph.component';
+import {FormsModule} from '@angular/forms';
+import {NgxCsvParserModule} from 'ngx-csv-parser';
+import {HighchartsChartModule} from 'highcharts-angular';
+import {GraphComponent} from './components/graph/graph.component';
 
 @NgModule({
   declarations: [
@@ -28,8 +26,7 @@ import { GraphComponent } from './components/graph/graph.component';
     FileDragNDropDirective,
     DropZoneComponent,
     DialogConfirmComponent,
-    TableComponent,
-    GraphComponent
+    GraphComponent,
   ],
   imports: [
     BrowserModule,
@@ -50,4 +47,5 @@ import { GraphComponent } from './components/graph/graph.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/app/services/algorithm.service.ts
+++ src/app/services/algorithm.service.ts
@@ -1,9 +1,7 @@
-import { Injectable } from '@angular/core';
-import { combineLatest } from 'rxjs';
-import { Observable, BehaviorSubject } from 'rxjs';
-import { NgxCsvParser } from 'ngx-csv-parser';
-import { NgxCSVParserError } from 'ngx-csv-parser';
-import { NgModule } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {BehaviorSubject, combineLatest} from 'rxjs';
+import {NgxCsvParser, NgxCSVParserError} from 'ngx-csv-parser';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -14,60 +12,79 @@ export class AlgorithmService {
   header = false;
   selectedChart = "";
   table = [];
+  response = {};
+  SERVER_URL = 'http://localhost:8080/detect';
   graphLabels$ = new BehaviorSubject<string[]>([]);
 
   private algorithm$ = new BehaviorSubject<string>('');
   private fileTest$ = new BehaviorSubject<File>(null);
   private fileTrain$ = new BehaviorSubject<File>(null);
+  private transformed1: string;
+  private transformed2: string;
 
-  constructor(private ngxCsvParser: NgxCsvParser) {
+  constructor(private ngxCsvParser: NgxCsvParser, private httpClient: HttpClient) {
 
   }
-  updateAlgorithm(alg: string){
+
+  postFile(file1: File, file2: File): void {
+    const formData: FormData = new FormData();
+    formData.append('kaki1', file1, file1.name);
+    formData.append('kaki2', file2, file2.name);
+    const upload$ = this.httpClient.post(this.SERVER_URL, formData, {
+      headers: new HttpHeaders({
+        'Content-Type': 'x-www-form-urlencoded'
+      })
+    });
+    upload$.subscribe();
+  }
+
+  updateAlgorithm(alg: string) {
     this.algorithm$.next(alg);
   }
 
-  updateFileTest(f: File){
-      this.fileTest$.next(f);
+  updateFileTest(f: File) {
+    this.fileTest$.next(f);
   }
 
-  updateFileTrain(f: File){
-      this.fileTrain$.next(f);
+  updateFileTrain(f: File) {
+    this.fileTrain$.next(f);
   }
 
   onUpload() {
     const f1: File = this.fileTest$.getValue();
     const f2: File = this.fileTrain$.getValue();
-
-     // Parse the file you want to select for the operation along with the configuration
-     combineLatest(
-        this.ngxCsvParser.parse(f1, { header: this.header, delimiter: ',' }),
-        this.ngxCsvParser.parse(f2, { header: this.header, delimiter: ',' }))
-        .subscribe(([result1, result2]: any) => {
-            this.table = result1;
-            this.graphLabels$.next(result1[0]);
-            // from here
-            console.log(this.algorithm$.getValue());
-            console.log('Transformed Result1', JSON.stringify(this.transformResult(result1)));
-            console.log('Transformed Result2', JSON.stringify(this.transformResult(result2)));
-          }, (error: NgxCSVParserError) => {
-            console.log('Error', error);
-     });
+    // Parse the file you want to select for the operation along with the configuration
+    combineLatest(
+      this.ngxCsvParser.parse(f1, {header: this.header, delimiter: ','}),
+      this.ngxCsvParser.parse(f2, {header: this.header, delimiter: ','}))
+      .subscribe(([result1, result2]: any) => {
+        this.table = result1;
+        this.graphLabels$.next(result1[0]);
+        // from here
+        console.log(this.algorithm$.getValue());
+        console.log('Transformed Result1', JSON.stringify(this.transformResult(result1)));
+        console.log('Transformed Result2', JSON.stringify(this.transformResult(result2)));
+        this.transformed1 = JSON.stringify(this.transformResult(result1));
+        this.transformed2 = JSON.stringify(this.transformResult(result2));
+      }, (error: NgxCSVParserError) => {
+        console.log('Error', error);
+      });
+    this.postFile(f1, f2);
   }
 
-   private transformResult(result: Array<any>):Array<any> {
-      const transformedResult = new Array<any>();
-      for (let i=0; i < result.length; i++) {
-          for (let j=0; j < result[i].length; j++) {
-            let rowResult: Array<any>;
-            if(i===0) {
-                rowResult = new Array<any>();
-                transformedResult.push(rowResult);
-            }
-            transformedResult[j].push(result[i][j]);
-          }
+  private transformResult(result: Array<any>): Array<any> {
+    const transformedResult = new Array<any>();
+    for (let i = 0; i < result.length; i++) {
+      for (let j = 0; j < result[i].length; j++) {
+        let rowResult: Array<any>;
+        if (i === 0) {
+          rowResult = new Array<any>();
+          transformedResult.push(rowResult);
+        }
+        transformedResult[j].push(result[i][j]);
       }
-      return transformedResult;
+    }
+    return transformedResult;
   }
 
 
